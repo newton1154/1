604ac829253a3cd6f48d67939d18115c9a0b224b
newton1154 <158603053+newton1154@users.noreply.github.com>
2024-09-12 11:38:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+Console.WriteLine("Введите номер операции:");
+Console.WriteLine("1. Сложение");
+Console.WriteLine("2. Вычитание");
+Console.WriteLine("3. Умножение");
+Console.WriteLine("4. Деление");
+start:
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int choice))
+{
+    Console.Write("Введите первое число: ");
+    int num1 = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите второе число: ");
+    int num2 = int.Parse(Console.ReadLine());
+
+    double result;
+    switch (choice)
+    {
+        case 1:
+            result = num1 + num2;
+            Console.WriteLine($"Результат: {num1} + {num2} = {result}");
+            break;
+        case 2:
+            result = num1 - num2;
+            Console.WriteLine($"Результат: {num1} - {num2} = {result}");
+            break;
+        case 3:
+            result = num1 * num2;
+            Console.WriteLine($"Результат: {num1} * {num2} = {result}");
+            break;
+        case 4:
+            if (num2 == 0)
+            {
+                Console.WriteLine("Ошибка: Деление на 0");
+            }
+            else
+            {
+                result = (double)num1 / num2;
+                Console.WriteLine($"Результат: {num1} / {num2} = {result}");
+            }
+            break;
+        default:
+            Console.WriteLine("Ошибка: Неверный номер операции");
+            break;
+
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка: Неверный ввод");
+}
+goto start;
